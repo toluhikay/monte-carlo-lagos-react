cb84702ee1d8277daa0ba4ae798ca2deb323b5c8
toluhikay <kayodesunday29@gmail.com>
2023-09-04 09:04:10 +0100

    monte carlo lagos picture updates

--- src/components/threePictures.js
+++ src/components/threePictures.js
@@ -1,31 +1,41 @@
 import React from "react";
-import First from "../assets/images/first.webp";
-import Second from "../assets/images/second.webp";
-import Third from "../assets/images/three.webp";
+import First from "../assets/images/main.jpeg";
+import Second from "../assets/images/second.jpeg";
+import Third from "../assets/images/third.jpeg";
+import Fourth from "../assets/images/fourth.jpeg";
+import Fifth from "../assets/images/fifth.jpeg";
+import sixth from "../assets/images/sixth.jpeg";
+import seventh from "../assets/images/seventh.jpeg";
+import first from "../assets/images/first.jpeg";
 import { commonStyle } from "../common/commonStyles";
 import { useDispatch } from "react-redux";
 import { SetModalOpen } from "../features/pictureModalSlice";
 
 const ThreePictures = () => {
-  const ImagesArray = [
-    { id: 1, img: First },
-    { id: 2, img: Second },
-    { id: 3, img: Third },
-  ];
-  const dispatch = useDispatch();
-  return (
-    <div className={`${commonStyle}`}>
-      <div className="flex justify-between flex-wrap">
-        {ImagesArray.map((item) => {
-          return (
-            <div data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000" key={item.id} className={`md:w-[32%] w-full md:mb-0 mb-12 md:border-[10px] border-[4px] rounded-lg`}>
-              <img src={item.img} alt="" onClick={() => dispatch(SetModalOpen(item.img))} />
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+	const ImagesArray = [
+		{ id: 1, img: First },
+		{ id: 2, img: Second },
+		{ id: 3, img: Third },
+		{ id: 4, img: Fourth },
+		{ id: 5, img: Fifth },
+		{ id: 6, img: sixth },
+		{ id: 7, img: seventh },
+		{ id: 8, img: first },
+	];
+	const dispatch = useDispatch();
+	return (
+		<div className={`${commonStyle}`}>
+			<div className='flex justify-between flex-wrap'>
+				{ImagesArray.map((item, index) => {
+					return (
+						<div data-aos={index % 2 === 0 ? "flip-left" : "flip-right"} data-aos-easing='ease-out-cubic' data-aos-duration='2000' key={item.id} className={`lg:w-[23%] md:w-[48%] w-full md:mb-12 mb-12 md:border-[10px] border-[4px] rounded-lg`}>
+							<img src={item.img} alt='' onClick={() => dispatch(SetModalOpen(item.img))} />
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
 };
 
 export default ThreePictures;
