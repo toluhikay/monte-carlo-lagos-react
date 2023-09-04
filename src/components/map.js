38e94f476310ec5f3106ca1487ab5efd73c578e0
toluhikay <kayodesunday29@gmail.com>
2023-09-04 19:39:29 +0100

    monte carlo main picture changes

--- src/components/map.js
+++ src/components/map.js
@@ -2,33 +2,24 @@ import React from "react";
 import { commonStyle, redColor, yellowColor } from "../common/commonStyles";
 
 const Map = () => {
-  return (
-    <div className={`${commonStyle}`}>
-      <div className="flex md:flex-row flex-col items-center py-12">
-        <div className="md:w-1/2 w-full flex md:text-start text-center flex-col justify-between h-full">
-          <p className={`text-[${redColor}] md:text-3xl text-2xl mb-6`}>
-            Monte <span className={`text-[${yellowColor}] `}>Carlo Lagos</span>
-          </p>
-          <p className="tracking-wider mb-6">This amazing estate comprises of 300sqm, 500sqm, 1000sqm and lake view plots overlooking the lagoon, with a boat ramp for navigating water recreational activities and transportation.</p>
-          <p className="mb-6">Initial Deposit</p>
-          <p className={`text-[${redColor}] md:text-2xl text-xl font-bold mb-6`}>₦4,000,000</p>
-          <p className={`tracking-widest md:text-2xl text-xl mb-6`}>Promo Ends 31st January 2023</p>
-        </div>
-        <div className={`md:w-1/2 w-full flex md:border-[5px] border-[4px] border-[#c3151690] rounded-lg justify-center items-center`}>
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.657168724022!2d3.486721514941358!3d6.438057125936457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bff7b601f46df%3A0x7b9ca29fb13ec179!2sAdozillion%20Homes%20and%20Realty!5e0!3m2!1sen!2sng!4v1675800694181!5m2!1sen!2sng"
-            className={`w-full `}
-            height="350"
-            title="Monte Carlo Lagos Map"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className={`${commonStyle}`}>
+			<div className='flex md:flex-row flex-col items-center py-12'>
+				<div className='md:w-1/2 w-full flex md:text-start text-center flex-col justify-between h-full'>
+					<p className={`text-[${redColor}] md:text-3xl text-2xl mb-6`}>
+						Monte <span className={`text-[${yellowColor}] `}>Carlo Lagos</span>
+					</p>
+					<p className='tracking-wider mb-6'>This amazing estate comprises of 300sqm, 500sqm, 1000sqm and lake view plots overlooking the lagoon, with a boat ramp for navigating water recreational activities and transportation.</p>
+					<p className='mb-6'>Initial Deposit</p>
+					<p className={`text-[${redColor}] md:text-2xl text-xl font-bold mb-6`}>₦2,000,000</p>
+					{/* <p className={`tracking-widest md:text-2xl text-xl mb-6`}>Promo Ends 31st January 2023</p> */}
+				</div>
+				<div className={`md:w-1/2 w-full flex md:border-[5px] border-[4px] border-[#c3151690] rounded-lg justify-center items-center`}>
+					<iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.657168724022!2d3.486721514941358!3d6.438057125936457!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bff7b601f46df%3A0x7b9ca29fb13ec179!2sAdozillion%20Homes%20and%20Realty!5e0!3m2!1sen!2sng!4v1675800694181!5m2!1sen!2sng' className={`w-full `} height='350' title='Monte Carlo Lagos Map' style={{ border: 0 }} allowFullScreen='' loading='lazy' referrerPolicy='no-referrer-when-downgrade'></iframe>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Map;
--- src/components/monteCarloSection.js
+++ src/components/monteCarloSection.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import Front from "../assets/images/front1.webp";
-import Valentine from "../assets/images/ValentineMC.webp";
-import Layout from "../assets/images/layout.webp";
+import Front from "../assets/images/MonteCarlo1.jpeg";
+import Valentine from "../assets/images/MonteCarlo2.jpeg";
+import Layout from "../assets/images/MonteCarlo3.jpeg";
 import { yellowColor, redColor, commonStyle } from "../common/commonStyles";
 import { ACTIONBUTTONS } from "../constants/constant";
 import { MdOutlineNavigateNext } from "react-icons/md";
@@ -10,50 +10,50 @@ import { SetModalOpen } from "../features/pictureModalSlice";
 import { useDispatch } from "react-redux";
 
 const MonteCarloSection = () => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  return (
-    <div className={`md:py-28 py-16 ${commonStyle} bg-[#c4c4c450]`}>
-      <p className={`md:text-5xl mb-12 mt-12 text-3xl font-bold text-[${redColor}] text-center`}>
-        Monte Carlo <span className={`text-[${yellowColor}]`}>Lagos</span>
-      </p>
-      <div className="w-full flex justify-center items-center mb-12">
-        <img src={Front} alt="" className="md:w-[40%] w-[80%]" />
-      </div>
-      <div className={`flex items-center flex-col py-12`}>
-        <p className={`py-6 font-bold md:text-3xl mb-12 text-[${yellowColor}] text-2xl`}>
-          Monte <span className={`text-[${redColor}]`}>Carlo Layout</span>
-        </p>
-        <div className={`w-full flex md:flex-row flex-col items-center justify-between`}>
-          <img src={Valentine} onClick={() => dispatch(SetModalOpen(Valentine))} className="md:w-[48%] w-[80%] md:mb-0 mb-12" alt="" />
-          <img src={Layout} onClick={() => dispatch(SetModalOpen(Layout))} className="md:w-[48%] w-[80%] " alt="" />
-        </div>
-        <div className="w-full flex flex-wrap justify-between mt-12">
-          {ACTIONBUTTONS.map((item, index) => {
-            return (
-              <button
-                key={index}
-                className={`md:w-[23%] w-[48%] md:mb-0 mb-6 bg-[${redColor}] flex md:text-sm text-xs justify-center hover:bg-[#e9a921]  items-center capitalize text-white cursor-pointer py-4`}
-                onClick={() => {
-                  if (item.link) {
-                    navigate(item.link);
-                  }
-                  if (item.fn) {
-                    dispatch(item.fn);
-                  }
-                  if (item.img) {
-                    dispatch(SetModalOpen(item.img));
-                  }
-                }}
-              >
-                {item.action} <MdOutlineNavigateNext className="text-xl" />
-              </button>
-            );
-          })}
-        </div>
-      </div>
-    </div>
-  );
+	const navigate = useNavigate();
+	const dispatch = useDispatch();
+	return (
+		<div className={`md:py-28 py-16 ${commonStyle} bg-[#c4c4c450]`}>
+			<p className={`md:text-5xl mb-12 mt-12 text-3xl font-bold text-[${redColor}] text-center`}>
+				Monte Carlo <span className={`text-[${yellowColor}]`}>Lagos</span>
+			</p>
+			<div className='w-full flex justify-center items-center mb-12'>
+				<img src={Front} alt='' className='md:w-[40%] w-[80%]' />
+			</div>
+			<div className={`flex items-center flex-col py-12`}>
+				<p className={`py-6 font-bold md:text-3xl mb-12 text-[${yellowColor}] text-2xl`}>
+					Monte <span className={`text-[${redColor}]`}>Carlo Layout</span>
+				</p>
+				<div className={`w-full flex md:flex-row flex-col items-center justify-between`}>
+					<img src={Valentine} onClick={() => dispatch(SetModalOpen(Valentine))} className='md:w-[48%] w-[80%] md:mb-0 mb-12' alt='' />
+					<img src={Layout} onClick={() => dispatch(SetModalOpen(Layout))} className='md:w-[48%] w-[80%] ' alt='' />
+				</div>
+				<div className='w-full flex flex-wrap justify-between mt-12'>
+					{ACTIONBUTTONS.map((item, index) => {
+						return (
+							<button
+								key={index}
+								className={`md:w-[23%] w-[48%] md:mb-0 mb-6 bg-[${redColor}] flex md:text-sm text-xs justify-center hover:bg-[#e9a921]  items-center capitalize text-white cursor-pointer py-4`}
+								onClick={() => {
+									if (item.link) {
+										navigate(item.link);
+									}
+									if (item.fn) {
+										dispatch(item.fn);
+									}
+									if (item.img) {
+										dispatch(SetModalOpen(item.img));
+									}
+								}}
+							>
+								{item.action} <MdOutlineNavigateNext className='text-xl' />
+							</button>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MonteCarloSection;
